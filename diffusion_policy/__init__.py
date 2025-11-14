aa2136c90b64acea55ef84571ee23bdfd40acafc
Elycyx <chen_yuxuan@sjtu.edu.cn>
2025-11-14 13:17:47 +0800

    update

--- diffusion_policy/__init__.py
+++ diffusion_policy/__init__.py
@@ -8,13 +8,12 @@ with force/torque integration.
 __version__ = '1.0.0'
 
 # Make key modules easily accessible
-from diffusion_policy.dataset import fadp_dataset, umi_dataset
+from diffusion_policy.dataset import fadp_dataset
 from diffusion_policy.policy import diffusion_unet_fadp_policy, diffusion_unet_timm_policy
 from diffusion_policy.model.vision import fadp_encoder, timm_obs_encoder
 
 __all__ = [
     'fadp_dataset',
-    'umi_dataset',
     'diffusion_unet_fadp_policy',
     'diffusion_unet_timm_policy',
     'fadp_encoder',
